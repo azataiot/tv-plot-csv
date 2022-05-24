ecde5a73cf3cc2ddc2c936c0c9e68ee880dbbcc6
Azat <8280770+azataiot@users.noreply.github.com>
2022-05-24 16:24:53 +0300

    before typescript

--- src/downloader.js
+++ src/downloader.js
@@ -1,19 +1,1 @@
-const { getHistoricalRates } = require("dukascopy-node");
 
-(async () => {
-  try {
-    const data = await getHistoricalRates({
-      instrument: "btcusd",
-      dates: {
-        from: new Date("2018-01-01"),
-        to: new Date("2019-01-01"),
-      },
-      timeframe: "d1",
-      format: "json",
-    });
-
-    console.log(data);
-  } catch (error) {
-    console.log("error", error);
-  }
-})();
